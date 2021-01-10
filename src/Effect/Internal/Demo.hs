77aee9c534e11b7bd62df84cca86c47b3c50701c
Evan Relf <evan@evanrelf.com>
2021-01-09 23:09:19 -0800

    Start conversion from `Sum f g` to `Union '[f, g]`

--- src/Effect/Internal/Demo.hs
+++ src/Effect/Internal/Demo.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+
 module Effect.Internal.Demo where
 
 import Control.Monad.Free (Free (..), runFree)
-import Data.Functor.Sum (Sum (..))
+import Data.Union (Union, Member (..))
 import Effect (sumToM)
 import Effect.Bell (Bell (..), bellToIO)
 import Effect.Teletype (Teletype (..), readLine, teletypeToIO, writeLine)
@@ -22,36 +25,36 @@ teletypeMain :: IO ()
 teletypeMain = runFree teletypeToIO teletypeProgram
 
 
-type TeletypeAndBell = Sum Teletype Bell
+type TeletypeAndBell = Union '[Teletype, Bell]
 
 
-readLine' :: Free TeletypeAndBell String
-readLine' = Free $ InL $ ReadLine pure
+readLine' :: Member Teletype es => Free (Union es) String
+readLine' = Free $ inject $ ReadLine pure
 
 
-writeLine' :: String -> Free TeletypeAndBell ()
-writeLine' message = Free $ InL $ WriteLine message (pure ())
+writeLine' :: Member Teletype es => String -> Free (Union es) ()
+writeLine' message = Free $ inject $ WriteLine message (pure ())
 
 
-ringBell' :: Free TeletypeAndBell ()
-ringBell' = Free $ InR $ RingBell (pure ())
+ringBell' :: Member Bell es => Free (Union es) ()
+ringBell' = Free $ inject $ RingBell (pure ())
 
 
-teletypeAndBellToIO :: TeletypeAndBell a -> IO a
-teletypeAndBellToIO = sumToM teletypeToIO bellToIO
+-- teletypeAndBellToIO :: TeletypeAndBell a -> IO a
+-- teletypeAndBellToIO = sumToM teletypeToIO bellToIO
 
 
-teletypeAndBellProgram :: Free TeletypeAndBell ()
-teletypeAndBellProgram = do
-  message <- readLine'
+-- teletypeAndBellProgram :: Free TeletypeAndBell ()
+-- teletypeAndBellProgram = do
+--   message <- readLine'
 
-  if message == "Ring the bell!" then do
-    ringBell'
-    writeLine' "Rang the bell!"
+--   if message == "Ring the bell!" then do
+--     ringBell'
+--     writeLine' "Rang the bell!"
 
-  else
-    writeLine' "Didn't ring the bell"
+--   else
+--     writeLine' "Didn't ring the bell"
 
 
-teletypeAndBellMain :: IO ()
-teletypeAndBellMain = runFree teletypeAndBellToIO teletypeAndBellProgram
+-- teletypeAndBellMain :: IO ()
+-- teletypeAndBellMain = runFree teletypeAndBellToIO teletypeAndBellProgram
