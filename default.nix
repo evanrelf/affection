28adb2fb9e86829a3f9deedb7b6df87e7de01429
Evan Relf <evan@evanrelf.com>
2021-05-14 12:44:04 -0700

    Fix Nix evaluation error, use overlay properly

--- default.nix
+++ default.nix
@@ -1,15 +1,17 @@
 let
-  haskellPackagesOverlay =
-    import ./nix/override-haskell-packages.nix {
-      packages = {
-        "affection" = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
-      };
-      overrides = { };
-      hackage = {
-        rev = "52415450270fb5d146097c36e74d1117ba0e4fe4";
-        sha256 = "0cc7ls5awhb83jfm8kcaskglgqala32q5s8j87frz0f6wx57gbl4";
-      };
-    };
+  haskellPackagesOverlay = pkgsFinal: pkgsPrev:
+    import ./nix/override-haskell-packages.nix
+      {
+        packages = {
+          "affection" = pkgsPrev.nix-gitignore.gitignoreSource [ ] ./.;
+        };
+        hackage = {
+          rev = "52415450270fb5d146097c36e74d1117ba0e4fe4";
+          sha256 = "0cc7ls5awhb83jfm8kcaskglgqala32q5s8j87frz0f6wx57gbl4";
+        };
+      }
+      pkgsFinal
+      pkgsPrev;
 
 
   pkgs = import ./nix/nixpkgs.nix { overlays = [ haskellPackagesOverlay ]; };
