7ffff4474e943035df08e29a0a54ffe47ce2b4c8
Evan Relf <evan@evanrelf.com>
2021-05-14 12:41:40 -0700

    Format Nix code with `nixpkgs-fmt`

--- default.nix
+++ default.nix
@@ -2,9 +2,9 @@ let
   haskellPackagesOverlay =
     import ./nix/override-haskell-packages.nix {
       packages = {
-        "affection" = pkgs.nix-gitignore.gitignoreSource [] ./.;
+        "affection" = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
       };
-      overrides = {};
+      overrides = { };
       hackage = {
         rev = "52415450270fb5d146097c36e74d1117ba0e4fe4";
         sha256 = "0cc7ls5awhb83jfm8kcaskglgqala32q5s8j87frz0f6wx57gbl4";
@@ -15,4 +15,4 @@ let
   pkgs = import ./nix/nixpkgs.nix { overlays = [ haskellPackagesOverlay ]; };
 
 in
-  pkgs.haskellPackages.affection
+pkgs.haskellPackages.affection
--- shell.nix
+++ shell.nix
@@ -1,12 +1,12 @@
 let
-  pkgs = import ./nix/nixpkgs.nix {};
+  pkgs = import ./nix/nixpkgs.nix { };
 
   affection = import ./default.nix;
 
 in
-  affection.env.overrideAttrs (old: {
-    buildInputs = with pkgs; old.buildInputs ++ [
-      cabal-install
-      ghcid
-    ];
-  })
+affection.env.overrideAttrs (old: {
+  buildInputs = with pkgs; old.buildInputs ++ [
+    cabal-install
+    ghcid
+  ];
+})
