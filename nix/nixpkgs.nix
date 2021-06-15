7a446b2680e73ee5659d51e21ffb75fa4481a7d7
Evan Relf <evan@evanrelf.com>
2021-06-15 11:58:36 -0700

    Bump Nixpkgs

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,9 +1,9 @@
 args:
 
 let
-  # master on 2021-01-03
-  rev = "1d10a2af91d31e0da00b3259cc976c7c0033ba85";
-  sha256 = "05iday54fvsrpri8sdmib59w1kq2afwd2zxwgdrhwiaj77s676fc";
+  # master on 2021-05-27
+  rev = "339a49503ae85b6cfd110b248010d24252d7aa45";
+  sha256 = "0zkljhm1v7iwwa06i62s2s8rbdcbc28nf3ps838c0jv3v32g4ws8";
 
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
